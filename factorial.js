dc7919583f58d5ffa53397d28d86d634ddfd6fe3
Cameron Hatch <cameronlavigne92@gmail.com>
2018-11-05 12:57:07 -0700

    iterative factorial

--- factorial.js
+++ factorial.js
@@ -6,3 +6,12 @@ const factorial = (num) => {
 };
 
 console.log(factorial(5));
+
+const iterativeFactorial = (num) => {
+  let prod = 1;
+  for (let i=1; i<=num; i++){
+    prod *= i;
+  }
+  return prod;
+};
+console.log(iterativeFactorial(5));
