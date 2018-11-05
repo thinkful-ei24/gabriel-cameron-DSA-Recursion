fe97be2d9ed25c41b83fe49c6263c06fffa082a3
Cameron Hatch <cameronlavigne92@gmail.com>
2018-11-05 13:32:18 -0700

    iterative anagram

--- anagram.js
+++ anagram.js
@@ -16,3 +16,39 @@ const allAnagrams = word => {
 };
 
 allAnagrams('east');
+
+function swap(chars, i, j) {
+  var tmp = chars[i];
+  chars[i] = chars[j];
+  chars[j] = tmp;
+}
+
+function iterativeAnagrams(input) {
+  var counter = [],
+    anagrams = [],
+    chars = input.split(''),
+    length = chars.length,
+    i;
+
+  for (i = 0; i < length; i++) {
+    counter[i] = 0;
+  }
+
+  anagrams.push(input);
+  i = 0;
+  while (i < length) {
+    if (counter[i] < i) {
+      swap(chars, i % 2 === 1 ? counter[i] : 0, i);
+      counter[i]++;
+      i = 0;
+      anagrams.push(chars.join(''));
+    } else {
+      counter[i] = 0;
+      i++;
+    }
+  }
+
+  return anagrams;
+}
+
+console.log(iterativeAnagrams('abc'));
