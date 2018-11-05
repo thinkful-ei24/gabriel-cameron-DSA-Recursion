a7161bebae6e701b91fe8f99d63ffeb9e6850c60
Gabriel Morris <gabemorris@outlook.com>
2018-11-05 12:09:40 -0800

    fibonacci

--- anagram.js
+++ anagram.js
@@ -1,19 +1,18 @@
-const allAnagrams = (word) => {
+const allAnagrams = word => {
   const results = {};
-  const anagram = (word, ana = '' ) => {
-    if(word === ''){
+  const anagram = (word, ana = '') => {
+    if (word === '') {
       results[ana] = '';
       return;
     }
     // loop through word so each letter is a prefix
-    for(let i=0; i<word.length; i++){
-      let newStr = word.slice(0, i) + word.slice(i+1, word.length);
+    for (let i = 0; i < word.length; i++) {
+      let newStr = word.slice(0, i) + word.slice(i + 1, word.length);
       anagram(newStr, ana + word[i]);
     }
   };
   anagram(word, '');
   console.log(Object.keys(results));
 };
-
 
 allAnagrams('east');
--- fibonacci.js
+++ fibonacci.js
@@ -6,4 +6,20 @@ function fibonacci(num) {
   return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-console.log(fibonacci(5));
+// console.log(fibonacci(5));
+
+function iterativeFibonacci(num) {
+  let prevNumOne = 1;
+  let prevNumTwo = 1;
+  let fiboNum;
+
+  for (let i = 3; i <= num; i++) {
+    fiboNum = prevNumOne + prevNumTwo;
+
+    prevNumOne = prevNumTwo;
+    prevNumTwo = fiboNum;
+  }
+  return fiboNum;
+}
+
+console.log(iterativeFibonacci(5));
