0b645e4c4ffb3bc688e20a67bc0d4c30457f11ac
Gabriel Morris <gabemorris@outlook.com>
2018-11-05 08:36:40 -0800

    array doubling

---
+++ arrayDouble.js
@@ -0,0 +1,17 @@
+/*
+Input: [1, 2, 3]
+Output: [2, 4, 6]
+*/
+
+function double(array) {
+  if (array.length === 0) {
+    return [];
+  }
+
+  const doubledNumber = array[0] * 2;
+
+  return [doubledNumber, ...double(array.slice(1))];
+}
+
+console.log(double([1, 2, 3]));
+console.log(double([2, 4, 6]));
